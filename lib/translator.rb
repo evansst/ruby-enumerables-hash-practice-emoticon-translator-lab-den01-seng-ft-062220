1eb4b81f01e0f74faf7cc36d1b5ddae3292aa96a
Sam Evans <evansst@icloud.com>
2020-06-16 05:46:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,8 +17,8 @@ def get_japanese_emoticon(path,emoticon)
   data = load_library(path)
   data.each do |name, element|
     binding.pry
-    if name[:english] == emoticon
-      return name[:japanese]
+    if element[:english] == emoticon
+      return element[:japanese]
     end
   end
   
